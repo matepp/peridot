6c54722f458dbaf488d9c281c33b36ff5b9653f7
Máté Papp <papp.mate@sonrisa.hu>
2023-06-24 14:58:54 +0200

    remove test data from code

--- cypress/e2e/second-task/bank.cy.js
+++ cypress/e2e/second-task/bank.cy.js
@@ -1,9 +1,6 @@
 import {depositAndValidateMessage, validateBalanceValue} from "../../support/pages/deposit";
 import {validateSumOfTransactions} from "../../support/pages/transaction";
 
-const depositValues = [100,10,5]
-const depositSum = 115
-
 describe('Task two', () => {
     //Implemented with a normal login flow but loading the local storage with the user data is also a possibility
     beforeEach(() => {
